bd0fb9afab915519b8c8be167d8635a6e47abf46
Dylan Parker <dylanparker@Dylans-MBP.attlocal.net>
2021-02-14 12:39:30 -0600

    New react front end.

--- app.py
+++ app.py
@@ -1,54 +1,83 @@
 # app.py
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, send_from_directory, request, jsonify, make_response
+from flask_cors import CORS, cross_origin
 from wordcloud import WordCloud
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt, mpld3
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='client/build', static_url_path='')
+cors = CORS(app)
 
 @app.route('/api/', methods=['GET'])
+@cross_origin()
 def respond():
     return "Welcome to the API!!!"
 
 
+@app.route('/api/justpie/')
+@cross_origin()
+def generatePie():
+    # Get the input data (Wedge is the distance between slices)
+    data = request.args.get('data')
+    colors = request.arts.get('colors')
+    wedge = request.args.get('wedge')
+        
+    # Turn it into a list
+    data = [float(i) for i in data.split(',')]
+    colors = ['#'+i for i in colors.split(',')]
+
+    # Make a matplotlib pie chart
+    fig1, ax1 = plt.subplots(figsize=(20, 20))
+    patches, texts = ax1.pie(data, 
+                             explode=[float(wedge) for w in range(0, len(data))],
+                             colors=colors,
+                             startangle=90)
+    
+    # Equal aspect ratio
+    ax1.axis('equal')
+    plt.tight_layout()
+
+    # Save the impage temporary on the local machine
+    plt.savefig(os.getcwd() + '/test.png')
+
+
 @app.route('/api/wordcloud/', methods=['GET'])
+@cross_origin()
 def generateWordCloud():
     # Get the input data (Wedge is the distance between slices)
     data = request.args.get('data')
-    print(data)
+    colors = request.arts.get('colors')
+    wedge = request.args.get('wedge')
+    
+    # Turn it into a list
+    data = [float(i) for i in data.split(',')]
+    colors = ['#'+i for i in colors.split(',')]
 
     # Generate the word cloud object   
     wordcloud = WordCloud().generate(str(data))
-
-    # Create and save the image to the current working directory
-    image = wordcloud.to_image()
-    image.show()
-    # image.save(os.getcwd() + '/test.png')
     
-    """
-
     # Plot configurations
+    plt.switch_backend('Agg')
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis("off")
 
     # Show the plot
-    plt.show()
+    mpld3.show()
 
     # Save the image temporary on the local machine
-    plt.savefig(os.getcwd() + '/test.png')
-
-    """
+    # plt.savefig(os.getcwd() + '/test.png')
 
     response = {}
     response["SUCCESS"] = f"Your word cloud was downloaded!!"
     return jsonify(response)
 
+
 @app.route('/')
-def index():
-    return render_template("index.html")
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(host='0.0.0.0')
 
